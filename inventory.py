af7f0b1b2aa648273d46a34522fac7989548593b
marek-matys <marek.matys@gmail.com>
2024-01-05 17:34:04 +0100

    rework

--- inventory.py
+++ inventory.py
@@ -139,13 +139,13 @@ if __name__ == "__main__":
 		logging.debug("Main    : thread %d done", index)
 
 with open('output.csv', 'w', newline='') as csvfile:
-	fieldnames = ['ServiceName','Global'] + allRegions
+	fieldnames = ['ServiceName','Global-Total'] + allRegions
 	print("Header:", fieldnames)
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	for i in dict:
-		print("Dict[i]:{0} i:{1}".format(dict[i],i))		
-		writer.writerow(dict[i])
+		print("Dict[i]:{0} i:{1} keys()".format(dict[i],i,dict[i].Keys()]))		
+		writer.writerow(dict[i].Keys())
     
 print(dict)
 
