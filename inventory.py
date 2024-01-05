96c5269198ee7ee03d2c8ac7b664508e3d73b091
marek-matys <marek.matys@gmail.com>
2024-01-05 17:35:39 +0100

    rework

--- inventory.py
+++ inventory.py
@@ -144,8 +144,8 @@ with open('output.csv', 'w', newline='') as csvfile:
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	for i in dict:
-		print("Dict[i]:{0} i:{1} keys()".format(dict[i],i,dict[i].Keys()))		
-		writer.writerow(dict[i].Keys())
+		#print("Dict[i]:{0} i:{1} keys()".format(dict[i],i,dict[i].Keys()))		
+		writer.writerow(i)
     
 print(dict)
 
