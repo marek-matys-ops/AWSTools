75d437b654c973b322672822d916cf0f73b28458
marek-matys <marek.matys@gmail.com>
2024-01-06 00:07:24 +0100

    rework

--- inventory.py
+++ inventory.py
@@ -64,30 +64,30 @@ def get_client(service_name,region_name):
 	except:
 		return None
 services = [	
-	["ec2", "EC2-Instances", "describe_instances()", False, "Reservations",None],
-	["s3", "S3", "list_buckets()", False, "Buckets",None],
-	["ec2", "EC2-VPC", "describe_vpcs()", False, "Vpcs",None],
-	["ec2", "EC2-VPN", "describe_vpn_connections()", False, "VpnConnections",None],
-	["ec2", "EC2-SUBNETS", "describe_subnets()", False, "Subnets",None],
-	["ec2", "EC2-SG", "describe_security_groups()", False, "SecurityGroups",None],
-	["r53", "R53", "get_hosted_zone_count()", True, "HostedZoneCount"],
-	["acm", "ACM", "list_certificates()", False, "CertificateSummaryList"],
-	["apigatewayv2", "API_GW_HTTP", "get_apis()", False, "Items",None],
-	["apigateway", "API_GW_EDGE", "get_rest_apis()", False, "items",None],
-	["lambda", "LAMBDA", "list_functions()", False, "Functions",None],
+	["ec2", "EC2-Instances", "describe_instances()", False, "Reservations",None,{}],
+	["s3", "S3", "list_buckets()", False, "Buckets",None,{}],
+	["ec2", "EC2-VPC", "describe_vpcs()", False, "Vpcs",None,{}],
+	["ec2", "EC2-VPN", "describe_vpn_connections()", False, "VpnConnections",None,{}],
+	["ec2", "EC2-SUBNETS", "describe_subnets()", False, "Subnets",None,{}],
+	["ec2", "EC2-SG", "describe_security_groups()", False, "SecurityGroups",None,{}],
+	["r53", "R53", "get_hosted_zone_count()", True, "HostedZoneCount",None,{}],
+	["acm", "ACM", "list_certificates()", False, "CertificateSummaryList",None,{}],
+	["apigatewayv2", "API_GW_HTTP", "get_apis()", False, "Items",None,{}],
+	["apigateway", "API_GW_EDGE", "get_rest_apis()", False, "items",None,{}],
+	["lambda", "LAMBDA", "list_functions()", False, "Functions",None,{}],
 	["cognito-identity", "COGNITO-identity", "list_identity_pools(MaxResults=60)", False, "IdentityPools",None, {"next_token_name":"NextToken"}],
-	["cognito-idp", "COGNITO-idp", "list_user_pools(MaxResults=60)", False, "UserPools",None],
-	["ecs", "ECS", "list_clusters()", False, "clusterArns",None],
-	["ecr", "ECR", "describe_repositories()", False, "repositories",None],
-	["elb", "ELB", "describe_load_balancers()", False, "LoadBalancerDescriptions",None],
-	["elbv2", "ELBv2", "describe_load_balancers()", False, "LoadBalancers",None],
-	["elasticbeanstalk", "ELBEANSTALK-env", "describe_environments()", False, "Environments",None],
-	["elasticbeanstalk", "ELBEANSTALK-app", "describe_applications()", False, "Applications",None],
-	["kms", "KMS", "list_keys()", False, "Keys",None],	
-	["rds", "RDS", "describe_db_instances()", False, "DBInstances",None],
-	["secretsmanager", "SECRETSMANAGER", "list_secrets()", False, "SecretList",None],
-	["sns", "SNS", "list_topics()", False, "Topics",None],
-	["cloudfront", "CLOUDFRONT", "list_distributions()", True, "DistributionList","Quantity"],
+	["cognito-idp", "COGNITO-idp", "list_user_pools(MaxResults=60)", False, "UserPools",None,{}],
+	["ecs", "ECS", "list_clusters()", False, "clusterArns",None,{}],
+	["ecr", "ECR", "describe_repositories()", False, "repositories",None,{}],
+	["elb", "ELB", "describe_load_balancers()", False, "LoadBalancerDescriptions",None,{}],
+	["elbv2", "ELBv2", "describe_load_balancers()", False, "LoadBalancers",None,{}],
+	["elasticbeanstalk", "ELBEANSTALK-env", "describe_environments()", False, "Environments",None,{}],
+	["elasticbeanstalk", "ELBEANSTALK-app", "describe_applications()", False, "Applications",None,{}],
+	["kms", "KMS", "list_keys()", False, "Keys",None,{}],	
+	["rds", "RDS", "describe_db_instances()", False, "DBInstances",None,{}],
+	["secretsmanager", "SECRETSMANAGER", "list_secrets()", False, "SecretList",None,{}],
+	["sns", "SNS", "list_topics()", False, "Topics",None,{}],
+	["cloudfront", "CLOUDFRONT", "list_distributions()", True, "DistributionList","Quantity",{}],
 ]  	
 
 def get_service_count(service_name, common_name, region_name, method_to_invoke, is_global, response_1level, response_2level=None,**kwargs):
@@ -165,12 +165,12 @@ if __name__ == "__main__":
 
 		for region in allRegions:
 			if service[3] == True: # One pass for global services
-				x = threading.Thread(target=get_service_count, args=(service[0], service[1], "us-east-1", service[2], service[3], service[4], service[5]))				
+				x = threading.Thread(target=get_service_count, args=(service[0], service[1], "us-east-1", service[2], service[3], service[4], service[5],service[6]))				
 				threads.append(x)
 				x.start()				
 				break
 			else:
-				x = threading.Thread(target=get_service_count, args=(service[0], service[1], region, service[2], service[3], service[4], service[5]))
+				x = threading.Thread(target=get_service_count, args=(service[0], service[1], region, service[2], service[3], service[4], service[5],service[6]))
 				threads.append(x)
 				x.start()
 
